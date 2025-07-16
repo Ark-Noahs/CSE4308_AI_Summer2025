9e0f2a5a02ae62f76bc563bb7570e1fe88f5c8e7
Noe chairez <noe00chairez@gmail.com>
2025-07-16 12:42:12 -0500

    initial commit for assignment

---
+++ Desktop/CSE4308_AI/red_blue_nim.py
@@ -0,0 +1,27 @@
+# noe chairez 
+# 1002018540
+# CSE4308_002 --> Summer 2025 Course 
+
+'''
+Program has use play against computer where each take a turm removing marble. Has two versions (standard game and mis�re)
+Rules:
+    -each player can remove 1 or 2 marbles 
+    -standard version: loser loses game when pile is empty on their turn 
+    -mis�re version: winner wins whenpile is empty on their turn //^^opposite of 
+
+
+
+*MUST be able to run w/ following arguments from command line:
+                red_blue_nim.py <num-red> <num-blue> [-version] [-first-player] [-d <depth>]
+
+                <num-red>      =     sets num of red marbles 
+                <num-blue>     =     sets num of blue marbles 
+                -<version>     =     *default: player loses if pile is empty  <--no input 
+                                     *-m     : misere version
+
+                -<first-player>=     *default: -h will allow user to go first
+                -d <depth>     =     EXTRA CREDIT OPTIONAL! --> use for depth limit search
+
+
+
+'''
